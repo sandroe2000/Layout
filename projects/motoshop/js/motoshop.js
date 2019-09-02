11d87daec5b80bcf3025cbf47e4c954900a2fb58
Sandro Espindula <sandroespindula@plusoft.com.br>
2019-09-02 19:43:28 -0300

    Template application CRM Lite

--- projects/motoshop/js/motoshop.js
+++ projects/motoshop/js/motoshop.js
@@ -1,1 +1,18 @@
 'use strict';
+
+// Toolbar extra buttons
+let btnFinish = $('<button></button>').text('Save').addClass('btn btn-info').on('click', function(){ 
+    alert('Finish Clicked'); 
+});
+
+let btnCancel = $('<button></button>').text('Cancel').addClass('btn btn-danger').on('click', function(){ 
+    $('#smartwizard').smartWizard("reset"); 
+});
+
+$('#smartwizard').smartWizard({
+    theme:'dots',
+    toolbarSettings: {
+        toolbarButtonPosition: 'end',
+        toolbarExtraButtons: [btnFinish, btnCancel]
+    }
+});
